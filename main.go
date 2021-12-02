b21e553bb58f015cb96d29a4feb18a95079fd344
liangpeng <liangpeng@moqipobing.com>
2021-12-02 22:40:59 +0800

    leet code

--- main.go
+++ main.go
@@ -1,10 +1,27 @@
 package main
 
 func main() {
-	fore := NewListNode(4, nil)
-	three := NewListNode(3, fore)
-	two := NewListNode(2, three)
-	one := NewListNode(1, two)
+	//fore := NewListNode(1, nil)
+	//three := NewListNode(2, fore)
+	//two := NewListNode(2, three)
+	//one := NewListNode(1, two)
+	//
+	//fmt.Println(IsPalindrome(one))
 
-	ReverseBetween(one, 1, 2)
+	//XX253()
+
+	//RemoveKdigits("123", 1)
+
+	//xx := []int{3,2,2,3}
+	//RemoveElement(xx, 3)
+
+	//fmt.Println(RemoveKdigits("2", 1))
+
+	flower := []int{0, 1, 0}
+	CanPlaceFlowers(flower, 1)
+}
+
+func XX253() {
+	xx := [][]int{{1, 5}, {8, 9}, {8, 9}}
+	MinMeetingRooms(xx)
 }
