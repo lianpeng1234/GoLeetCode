b03b36e6fa19f12b74d7b2a9f3674ef1052bcb75
liangpeng <liangpeng@moqipobing.com>
2021-12-16 21:49:38 +0800

    leet code

--- main.go
+++ main.go
@@ -3,46 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	//fore := NewListNode(1, nil)
-	//three := NewListNode(2, fore)
-	//two := NewListNode(2, three)
-	//one := NewListNode(1, two)
-	//
-	//fmt.Println(IsPalindrome(one))
 
-	//XX253()
+//["MyCircularDeque","insertFront","deleteLast","getRear","getFront","getFront","deleteFront","insertFront","insertLast","insertFront","getFront","insertFront"]
+//	[[4],                [9],            [],        [],       [],         [],           [],       [6],           [5],         [9],         [],        [6]]
 
-	//RemoveKdigits("123", 1)
-
-	//xx := []int{3,2,2,3}
-	//RemoveElement(xx, 3)
-
-	//fmt.Println(RemoveKdigits("2", 1))
-
-	//flower := []int{0, 1, 0}
-	//CanPlaceFlowers(flower, 1)
-
-	//xx := [][]int{{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}}
-	//xx := [][]int{{},{1},{},{2},{}}
-	//xx := [][]int{{-1,3}}
-	//target := 3
-	//var b = FindNumberIn2DArray(xx, target)
-	//fmt.Println(b)
-
-	xx := []int{73, 74, 75, 71, 69, 72, 76, 73}
-	yy := dailyTemperatures(xx)
-	fmt.Println(yy)
+	circularDeque := Constructor(4)           // 设置容量大小为3
+	fmt.Println(circularDeque.InsertFront(5))  // 返回 true
+	fmt.Println(circularDeque.InsertLast(7))  // 返回 true
+	fmt.Println(circularDeque.GetFront()) // 返回 true
+	fmt.Println(circularDeque.InsertFront(4)) // 已经满了，返回 false
+	fmt.Println(circularDeque.GetRear())      // 返回 2
+	fmt.Println(circularDeque.IsFull())       // 返回 true
+	fmt.Println(circularDeque.DeleteLast())   // 返回 true
+	fmt.Println(circularDeque.InsertFront(4)) // 返回 true
+	fmt.Println(circularDeque.GetFront())     // 返回 4
 }
-
-//func XX253() {
-//	xx := [][]int{{1, 5}, {8, 9}, {8, 9}}
-//	MinMeetingRooms(xx)
-//}
-
-//func swordOffer04()  {
-//	xx := [][]int{{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}}
-//	target := 5
-//	var b = FindNumberIn2DArray(xx, target)
-//	fmt.Println(b)
-//
-//}
