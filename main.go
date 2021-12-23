4124051c160957a1a1cb71b8e5dc5a01ffa42faa
liangpeng <liangpeng@moqipobing.com>
2021-12-23 13:12:57 +0800

    leet code

--- main.go
+++ main.go
@@ -4,17 +4,6 @@ import "fmt"
 
 func main() {
 
-//["MyCircularDeque","insertFront","deleteLast","getRear","getFront","getFront","deleteFront","insertFront","insertLast","insertFront","getFront","insertFront"]
-//	[[4],                [9],            [],        [],       [],         [],           [],       [6],           [5],         [9],         [],        [6]]
+	fmt.Println(strStr("", "a"))
 
-	circularDeque := Constructor(4)           // 设置容量大小为3
-	fmt.Println(circularDeque.InsertFront(5))  // 返回 true
-	fmt.Println(circularDeque.InsertLast(7))  // 返回 true
-	fmt.Println(circularDeque.GetFront()) // 返回 true
-	fmt.Println(circularDeque.InsertFront(4)) // 已经满了，返回 false
-	fmt.Println(circularDeque.GetRear())      // 返回 2
-	fmt.Println(circularDeque.IsFull())       // 返回 true
-	fmt.Println(circularDeque.DeleteLast())   // 返回 true
-	fmt.Println(circularDeque.InsertFront(4)) // 返回 true
-	fmt.Println(circularDeque.GetFront())     // 返回 4
 }
